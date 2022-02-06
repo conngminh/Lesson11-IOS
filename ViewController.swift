33a6800739f56d45e9fa19fb33ac25b59b49c8e1
conngminh <congminhhttt@gmail.com>
2022-02-06 21:31:13 +0700

    Tab gesture by code

--- ViewController.swift
+++ ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupGesture()
     }
 
-
+    func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapLogo))
+        imgLogo.isUserInteractionEnabled = true
+        imgLogo.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func tapLogo() {
+        let alert = UIAlertController(title: "You tab on logo by tap gesture in code", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Agree", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
