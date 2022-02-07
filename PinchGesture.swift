92069c6b30154dec301dd0d8368021308592527a
conngminh <congminhhttt@gmail.com>
2022-02-07 23:04:47 +0700

    Pinch gesture on interface

--- PinchGesture.swift
+++ PinchGesture.swift
@@ -10,7 +10,7 @@ import UIKit
 class PinchGesture: UIViewController {
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var imgBall: UIImageView!
-    
+    @IBOutlet weak var imgLogo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupPingGesture()
@@ -28,6 +28,15 @@ class PinchGesture: UIViewController {
             let y = pinch.scale
             view.transform = view.transform.scaledBy(x: x, y: y)
             pinch.scale = 1
+        }
+    }
+    
+    @IBAction func pinchOnLogo(_ sender: UIPinchGestureRecognizer) {
+        if let view = sender.view {
+            let x = sender.scale
+            let y = sender.scale
+            view.transform = view.transform.scaledBy(x: x, y: y)
+            sender.scale = 1
         }
     }
 }
